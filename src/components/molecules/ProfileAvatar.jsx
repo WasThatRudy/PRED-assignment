8b0a89683a8c02e675f8515e83f1228ddb293796
Rudraksha Singh Sengar <rudraksharss@gmail.com>
2025-11-04 16:14:24 +0530

    Refine ProfileAvatar component dimensions by adjusting height and width for better visual consistency. Removed unnecessary padding to streamline the design.

--- src/components/molecules/ProfileAvatar.jsx
+++ src/components/molecules/ProfileAvatar.jsx
@@ -9,7 +9,7 @@ import React from 'react';
  */
 export const ProfileAvatar = ({ src, onClick, className = '' }) => {
   return (
-    <div className="border border-border-light rounded-sm h-[1.875rem] w-[1.875rem] flex items-center justify-center p-1">
+    <div className="border border-border-light rounded-sm h-[1.75rem] w-[1.75rem] flex items-center justify-center">
       <div className={`relative w-[1.5rem] h-[1.5rem] rounded-full border-[1px] border-white overflow-hidden flex-shrink-0 ${className}`}>
         <div className="w-full h-full bg-gradient-avatar" />
       </div>
